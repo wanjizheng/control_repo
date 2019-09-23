2f627efa71f4a5bd7c6a7d14cdc0e1258bba6b23
Jizheng <wanjizheng@users.noreply.github.com>
2019-09-23 18:48:40 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,10 +9,13 @@ node 'master.puppet.vm' {
   }
 }
 
+node 'minetest.puppet.vm' {
+  include role::minecraft_server
+}
+
 node /^web/ {
   include role::app_server
 }
-
 
 node /^db/ {
   include role::db_server
