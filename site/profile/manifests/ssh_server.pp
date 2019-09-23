330741e14b26951e5151deb28158f84185bc8d2d
Jizheng <wanjizheng@users.noreply.github.com>
2019-09-23 18:05:15 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDJF8y840UsRa5qL1ZyHNplRE0mq6fDuN/rKbNcDyl9RMf1lcHdZzGAkpfR4GwkmCfcqmqzjc6j8D62xqKRy+yrYL2qB3ehwvBrMTtbv317tcOw4F/SOnEAL5EAyaZ2gjtrUqfPrAFVyx6TNCudaoeot2hjwYDEn35IXjs6F2SpYh5qB0t00WlPNb/tDzaVNyGuWG11enaI+O/mAvF7sppX3QFRIKhFFxIqztC7BRLuBNc1HiSo2CWXhwLwfxXfjeDvmB/MAdUMIEqykwTQdBGzCfn1s/Da8wFquarMX7w6bi9BF/t/nmxqGNhxC3DImpoOlApGXF4lFeU+LtQ45fk3',
   }  
 }
